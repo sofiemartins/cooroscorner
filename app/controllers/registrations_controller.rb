8a2bb74c4698ea4cd9b6495e58988cc8aedf44c3
Sofie Martins <sam700@hotmail.de>
2015-05-13 12:22:32 +0200

    Added sign up mechnism

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,22 @@
 class RegistrationsController < Devise::RegistrationsController
   clear_respond_to
   respond_to :json
+
+  def create
+    @user = User.new(user_params)
+    if user.save
+      log_in @user
+      flash[:success] = "Your account has been created successfully!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private 
+  
+    def user_params
+      params.require(:user).permit(:name, :email, 
+		:password, :password_confirmation)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :confirmable, :registerable,
+  devise :database_authenticatable , :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  attr_accessor :username
+  
+  def username
+  end
 
 end
