36b1d5b1968afbc0689a750556cf08f348f23896
Sofie Martins <sam700@hotmail.de>
2015-06-03 21:00:09 +0200

    Changed category routes from label to short

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -27,16 +27,55 @@ class CategoryController < ApplicationController
 
   def submit_edit
     if !current_user || !current_user.admin?
+      not_found
     else 
+      category = Category.find_by(:short => params[:short])    
+      evaluate_new_label_input(category)
+      evaluate_new_short_input(category)
+      evaluate_new_background_input(category)
+      category.save
     end
   end
 
-  private
+  def destroy
+    if !current_user || !current_user.admin?
+      not_found
+    else
+      category = Category.find_by(:short => params[:short])
+      category.delete
+    end
+  end
 
+  private 
+    def evaluate_new_label_input(category)
+      new_label = params[:edit][:label]
+      if !!new_label
+        category.label = new_label 
+      end
+    end
+
+  private 
+    def evaluate_new_short_input(category)
+      new_short = params[:edit][:short]
+      if !!new_short
+        category.short = new_short
+      end
+    end
+
+  private 
+    def evaluate_new_background_input(category)
+      new_file = params[:edit][:background]
+      if !!new_file
+        category.background = new_file
+      end
+    end
+
+  private
     def not_found
       raise ActionController::RoutingError.new('Not Found')
     end
  
+  private
     def save_category
       @category = Category.save(label: params[:category][:label],
 				background: params[:category][:background])
