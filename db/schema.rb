250c9093ddedcf34ff0c465216fa24e5d373558b
Sofie Martins <sofiemartins@web.de>
2015-06-16 04:20:16 +0200

    messing with the custom.scss

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616001637) do
+ActiveRecord::Schema.define(version: 20150616011046) do
 
   create_table "backgrounds", force: :cascade do |t|
     t.string   "label"
@@ -47,12 +47,12 @@ ActiveRecord::Schema.define(version: 20150616001637) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 20150616001637) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
-    t.boolean  "admin",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
