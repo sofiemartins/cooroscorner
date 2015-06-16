800bf76d0a5f7abad94dec75d3a4353a92fa5b8f
Sofie Martins <sofiemartins@web.de>
2015-06-16 02:51:17 +0200

    can't make back or next when first or last comic respectively

--- app/helpers/comics_helper.rb
+++ app/helpers/comics_helper.rb
@@ -1,2 +1,15 @@
 module ComicsHelper
+
+  def last_comic
+    if !params[:category]
+      last = (params[:index].to_i == Comic.all.count)
+    else
+      last = (params[:index].to_i == Comic.where(:category => params[:category]).count)
+    end
+  end
+
+  def first_comic
+    params[:index].to_i == 1
+  end
+
 end
