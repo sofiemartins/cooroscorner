bda74e74e751a3d1a134539769fc5bcc6e837a6c
Sofie Martins <sofiemartins@web.de>
2015-06-17 11:24:33 +0200

    authors_comment is now text instead of string

--- db/migrate/20150519043146_create_comics.rb
+++ db/migrate/20150519043146_create_comics.rb
@@ -2,7 +2,7 @@ class CreateComics < ActiveRecord::Migration
   def change
     create_table :comics do |t|
       t.string :title
-      t.string :authors_comment
+      t.text :authors_comment
 
       t.timestamps null: false
     end
