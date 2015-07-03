91cea669e65e111934967c20e24fb5fe88a3a6e4
Matkin Gusev <matkin@sputnik.sputnik>
2015-07-04 00:07:08 +0200

    removed 404 as controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  match "/404" => "errors#error404", via: [ :get, :post, :patch, :delete ]
-
   devise_for :user, :controllers => {
     :sessions => 'user/sessions', 
     :registrations => 'user/registrations'
