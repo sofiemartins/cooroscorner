6ce95fc013c6bf89dc72b1a802df41d37a736ce5
Matkin Gusev <matkingusev@hotmail.de>
2015-07-11 23:22:51 +0200

    modifired gitignore

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,18 +1,22 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :application, 'cooroscorner'
+set :application, 'cooroscorner.com'
 set :deploy_user, 'web45'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :deploy_to 'server5.'
+
+set :scm, :git
+set :repo_url, 'git@github.com:sofiemartins/cooroscorner.git'
+
+set :keep_releases, 5
+set :linked_files, %w{config/database.yml}
+set :linkes_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
-
-# Default value for :scm is :git
-# set :scm, :git
+set :deploy_to, '/var/www/my_app'
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -23,19 +27,11 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # Default value for :pty is false
 # set :pty, true
 
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
 namespace :deploy do
+
+  before :deploy, "deploy:run_tests"
+  after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
+  after :finishing, 'deploy:cleanup'
 
   desc 'Restart application'
   task :restart do
